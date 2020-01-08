91225e51936f89815bf8c3bffb2011f0c2a319ba
aya <aya.yoshida8@gmail.com>
2020-01-08 16:44:20 -0500

    comments added

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,5 +1,31 @@
 var date = $(".date");
+var event = $("#text").value;
+var saveBtn = $("#saveBtn");
+var clearBtn = $("#clearBtn");
 
-date = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(date);
-$(".date").text();
+// For date and time
+var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$(date).text(todaysDate);
+
+// Function for timeblock by hour
+var hours = ["9"]
+var hour = $(".hour");
+hour = "";
+
+for(var i = 0; i < hours.length; i++) {
+  $(hour).text(hours + 1);
+}
+
+// Function for save button
+saveBtn.on("click", function() {
+  console.log(event);
+  debugger
+  $("#text").text(localStorage.setItem("events", event));
+})
+
+
+// Funtcion for saving content into local storage 
+
+// Function for clear button, clearing content in field and in local storage
+
+// Function for changing color scheme when time is past, present, future
